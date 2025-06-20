ea6a210c992a24fe318dd967dd5ed99a4555232f
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-06-20 19:36:14 -0400

    exercise 4

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -1,5 +1,7 @@
 package labs_examples.fundamentals.labs;
 
+import java.util.Arrays;
+
 /**
  * Fundamentals Exercise 4: My Second Program
  *
@@ -16,6 +18,22 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte a = 127;
+        System.out.println("byte a is: " + a);
+        short b = 32767;
+        System.out.println("short b is: " + b);
+        int c = 100000;
+        System.out.println("int c is: " + c);
+        long d = 30000000;
+        System.out.println("long d is: " + d);
+        float e = 33.73f;
+        System.out.println("float e is: " + e);
+        double f = 383858.48484;
+        System.out.println("double f is: " + f);
+        boolean g = true;
+        System.out.println("boolean g is: " + g);
+        char h = 'H';
+        System.out.println("char h is: " + h);
 
     }
 
