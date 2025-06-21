f327f9bdfbd824de536801831580d7ec2aee28f3
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-06-21 15:01:31 -0400

    exercise 5

--- src/labs_examples/datatypes_operators/labs/Exercise_05.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_05.java
@@ -13,13 +13,33 @@ class LogicalOperators {
         // example of "OR"
         boolean a = true;
         boolean b = false;
-        if (a | b){
+        if (a | b) {
             System.out.println("a or b is true");
+
+            // write your code below
+            boolean c = true;
+            boolean d = true;
+            if (c & d) System.out.println("c and d are true, both evaluated");
+
+            boolean e = true;
+            boolean f = true;
+            if (e && f) System.out.println("e and f are true, e evaluated first, followed by f");
+
+            boolean g = false;
+            boolean h = true;
+            if (g | h) System.out.println("g or h is true");
+
+            int i = 10;
+            int j = 20;
+            if ((i > 5) || (j > 5)) System.out.println("i or j is greater than 5, i evaluated first");
+
+            boolean k = true;
+            boolean l = false;
+            if (k ^ l) System.out.println("k and l are different");
+
+            boolean m = true;
+            boolean n = false;
+            if (m=!n) System.out.println("m is not equal to n");
         }
 
-        // write your code below
-
-    }
-
-}
-
+    }}
