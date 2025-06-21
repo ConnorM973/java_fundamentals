480c83021f5c010665b0626f2733f19e63943d54
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-06-21 15:32:50 -0400

    exercise 5

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,20 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
-
+        int a = str.length();
         String str2 = "hello";
+        int b = str2.length();
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean c = a == b;
+        System.out.println("True or false: is str the same length as str2? " + c);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String str3 = str.concat(str2);
+        System.out.println(str3);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
-
+        String str4 = "Hi!";
+        String str5 = str4.replace('!','.');
+        System.out.println(str5);
     }
 
 
