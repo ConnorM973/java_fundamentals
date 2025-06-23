20d48d32b3def1b56b4811dfd0b0ae5376d30663
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-06-23 12:50:58 -0400

    datatypes operators- exercise 6

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -13,6 +13,12 @@ public class Exercise_06 {
     public static void main(String[] args) {
 
         // write code here
-
+        float radius = 3.14f;
+        float pi = radius;
+        byte height = 5;
+        double surfaceArea= (2*pi*radius*radius+(2*pi*radius*height));
+        double volume= (pi*radius*radius*height);
+        System.out.println("Surface Area Equals "+surfaceArea);
+        System.out.println("Volume Equals "+volume);
     }
 }
