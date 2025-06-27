a129ed3bfcd65851424e5fc2731525bc1dbbce02
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-06-26 21:34:13 -0400

    Conditions_loops exercise 2

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -14,8 +16,27 @@ package labs_examples.conditions_loops.labs;
 public class Exercise_02 {
 
     public static void main(String[] args) {
-    
+        Scanner scanner = new Scanner(System.in);
         // write completed here
-
+        System.out.println("Enter a number from 1-7");
+        int i = scanner.nextInt();
+        if (i ==1){
+            System.out.println("The day is Monday");
+        }
+        else if (i==2){
+            System.out.println("The day is Tuesday");
+        } else if (i==3) {
+            System.out.println("The day is Wednesday");
+        } else if (i==4){
+            System.out.println("The day is Thursday");
+        } else if (i==5){
+            System.out.println("The day is Friday");
+        } else if (i==6){
+            System.out.println("The day is Saturday");
+        } else if (i==7){
+            System.out.println("The day is Sunday");
+        } else{
+            System.out.println("That isn't a number from 1-7");
+        }
     }
 }
