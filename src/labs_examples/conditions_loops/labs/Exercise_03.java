6cddafc6dc847b25a98bf61fda7784a372eb0f4b
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-06-27 10:26:40 -0400

    conditions_loops exercise 3

--- src/labs_examples/conditions_loops/labs/Exercise_03.java
+++ src/labs_examples/conditions_loops/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 3: Months of the year
  *
@@ -9,5 +11,54 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_03 {
+        public static void main(String[] args) {
 
-}
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number from 1-12");
+        int i = scanner.nextInt();
+        String monthofYear;
+        switch(i){
+                case 1:
+                        monthofYear="January";
+                        break;
+                case 2:
+                        monthofYear="February";
+                        break;
+                case 3:
+                        monthofYear="March";
+                        break;
+                case 4:
+                        monthofYear="April";
+                        break;
+                case 5:
+                        monthofYear="May";
+                        break;
+                case 6:
+                        monthofYear="June";
+                        break;
+                case 7:
+                        monthofYear="July";
+                        break;
+                case 8:
+                        monthofYear="August";
+                        break;
+                case 9:
+                        monthofYear="September";
+                        break;
+                case 10:
+                        monthofYear="October";
+                        break;
+                case 11:
+                        monthofYear="November";
+                        break;
+                case 12:
+                        monthofYear="August";
+                        break;
+                default:
+                        monthofYear="N/A";
+                        break;
+
+
+        }
+        System.out.println("The month of the year is "+monthofYear);
+}}
