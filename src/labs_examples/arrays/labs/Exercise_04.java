a848dd4f6aab4494a46db410918931190865ecb6
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-07-01 23:10:35 -0400

    array exercise 4

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -1,13 +1,22 @@
 package labs_examples.arrays.labs;
 
 /**
- *  Irregular Arrays
- *
- *      Create and populate a 2 dimensional irregular array of size and contents of your choosing. Using a nested
- *      "for-each" loop, iterate and print out each element of the array.
- *
+ * Irregular Arrays
+ * <p>
+ * Create and populate a 2 dimensional irregular array of size and contents of your choosing. Using a nested
+ * "for-each" loop, iterate and print out each element of the array.
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
+        for (int[] numbers : matrix) {
+            for (int value : numbers) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+
+        }
+    }
 }
