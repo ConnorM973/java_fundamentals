71b28564515f13c1dc9df88da6b1555ec625a732
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-07-04 21:35:40 -0400

    conditions_loops exercise 11

--- src/labs_examples/conditions_loops/labs/Exercise_11.java
+++ src/labs_examples/conditions_loops/labs/Exercise_11.java
@@ -22,5 +22,14 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_11 {
-
+    public static void main(String[] args) {
+        int [][] table = new int[10][10];
+                for (int i=0;i<10;i++){
+                    for (int j=0;j<10;j++){
+                        table[i][j]=(i+1)*(j+1);
+                        System.out.print(table[i][j]+"\t");
+                    }
+                    System.out.println();
+                }
+    }
 }
