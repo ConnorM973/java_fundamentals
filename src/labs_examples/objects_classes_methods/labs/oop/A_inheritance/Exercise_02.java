930a5674e2a41f2aab0bb43d9acdeae2a6c931f9
Connor McCaffrey <connor.mccaffrey973@gmail.com>
2025-07-14 01:14:05 -0400

    OOP inheritance exercises 2-5

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -4,7 +4,7 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Run the class below and study the output. What prints and why?
  *
  * Your answer:
- *
+ *The new object, "a", is a reference to the A class. Java resolves the a.i reference based on the object of class A which was originally referenced, not the object type B.
  */
 class A {
     int i = 10;
